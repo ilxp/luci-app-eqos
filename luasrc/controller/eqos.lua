212d2572a19cd0566ea4bae57a033f1115fbb8c1
ilxp <61609258+ilxp@users.noreply.github.com>
2023-03-15 14:52:43 +0800

    Update eqos.lua

--- luasrc/controller/eqos.lua
+++ luasrc/controller/eqos.lua
@@ -5,8 +5,8 @@ function index()
 		return
 	end
 	
-	entry({"admin","QOS"}, firstchild(), "QOS", 88).dependent = false
-	local page = entry({"admin", "QOS", "eqos"}, cbi("eqos"), "EQoS")
+	entry({"admin","qos"}, firstchild(), "QOS", 88).dependent = false
+	local page = entry({"admin", "qos", "eqos"}, cbi("eqos"), "EQoS")
 	page.dependent = true
 	page.acl_depends = { "luci-app-eqos" }
 end
